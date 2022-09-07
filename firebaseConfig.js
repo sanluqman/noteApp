c6cddb662754a097750246a709d0f9efeffb46bf
sanluqman <sangaming50@gmail.com>
2022-09-07 18:22:49 +0300

    2nd commit

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,12 +5,12 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjzzSc4qCYtT6Kdjm6XXrnPlXFUwS_vLQ",
-  authDomain: "learning-firebase-8f0ab.firebaseapp.com",
-  projectId: "learning-firebase-8f0ab",
-  storageBucket: "learning-firebase-8f0ab.appspot.com",
-  messagingSenderId: "842228605707",
-  appId: "1:842228605707:web:8fe9e386ac6a0abf58b80e",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGEBOCKET,
+  messagingSenderId: process.env.MESSAG,
+  appId: process.env.APPID,
 };
 
 export const app = firebase.initializeApp(firebaseConfig);
